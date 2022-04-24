223b540513bcc75aba08fb579edd76ed5367eefa
RatonRoy <royraton18@gmail.com>
2022-04-24 20:29:18 +0600

    setup files

---
+++ _step.js
@@ -0,0 +1,18 @@
+/* ***********
+1. creat a folder or (mkdir folder name  with commend line )
+2. open command line then go to the folder.
+3. npm init 
+4. npm install express.
+5.creat index.js file
+---------------------
+Express or Node resources 
+--------------------
+1. require the express 
+2. declare the app variable 
+3. set the port where you want to show your website. 
+4. set app.get('/', a function (optional))
+5. set app.listen(port, a function)
+6. run node index.js 
+7. check your browser for that port 
+
+*************** */
---
+++ index.js
@@ -0,0 +1,15 @@
+// require is like import . here import express to the express bariable 
+const express = require('express');
+// deceler the express function 
+const app = express();
+// then declare the port variable where you want to live your website on that port .
+const port = process.env.PORT || 3000;
+// app.get() take two parameter first is a route /. and second is a function I can use this function write above or write directly here. this function take two argument first is req (short version of request ) and second is res (short version of the response )
+app.get('/', (req, res) => {
+	res.send(' here I am now trying to code in node');
+})
+// then use app.listen() that take two parameter one is port another is a optional function 
+app.listen(port, () => {
+	console.log('listening to port', port);
+})
+// then run command with node index.js 
